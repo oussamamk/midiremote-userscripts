9514bb17e8c72eee3e8831562aee9443f9ad52cf
Oussama Mkaabal <oussamamk@hotmail.com>
2025-12-03 23:48:08 +0200

    handle labels of mapped instruments

--- Combined/Controls/page_instrument.js
+++ Combined/Controls/page_instrument.js
@@ -1,6 +1,14 @@
+var helper = require("./helper.js")
+var sendLableApp1 = helper.sendLableApp1
+var sendLableApp2 = helper.sendLableApp2
+
+
 var page_common = require('./page_common.js')
 var makeSubPageTransportAndContols = page_common.makeSubPageTransportAndContols
 var bindInstrumentKnobsButtons = page_common.bindInstrumentKnobsButtons
+var resetLabels1 = page_common.resetLabels1
+var resetLabels2 = page_common.resetLabels2
+var updateEffectsKnobsButtons = page_common.updateEffectsKnobsButtons
 
 var instrumentsMapping = {
     // 'Kontakt 8': function () {
@@ -56,6 +64,7 @@ function getInstrumentMappping(name) {
         mapping = instrumentsMapping[name]()
     }
     else {
+        mapping.ignore = ['#', 'CC', 'Omni', 'Poly', 'Bank ', 'Foot ', 'Data ', 'Portamento ', 'Channel Volume', 'Balance(', 'Pan(', 'All ', 'Pitchbend', 'Expression(', ' Pedal', 'Sostenuto', 'Legato ', 'Hold ', 'Aftertouch', 'Kontakt']
         mapping.smapping = true
     }
     return mapping
@@ -102,18 +111,28 @@ function makePageInstrument(deviceDriver, context) {
     instrument.mOnChangePluginIdentity = function (activeDevice, activeMapping, name, vendor, version, format) {
         var arr = ["name:", name, "vendor:", vendor, "version:", version, "format:", format]
         console.log(arr.join(' '))
+        resetLabels1(context)
+        resetLabels2(context)
         if (instrumentsMapping[name]) {
             var mapping = getInstrumentMappping(name)
             bindInstrumentKnobsButtons(page, defaultSubPage, customVar, instrument, context, activeDevice, activeMapping, effectContext, mapping, false)
         }
+        updateEffectsKnobsButtons(context, activeDevice, effectContext)
+        sendLableApp1(activeDevice, context)
+        sendLableApp2(activeDevice, context)
     }
 
     page.mHostAccess.mTrackSelection.mMixerChannel.mOnTitleChange = function(activeDevice, activeMapping, title) {
         console.log("title: " + title)
+        resetLabels1(context)
+        resetLabels2(context)   
         if (instrumentsMapping[title]) {
             var mapping = getInstrumentMappping(title)
             bindInstrumentKnobsButtons(page, defaultSubPage, customVar, instrument, context, activeDevice, activeMapping, effectContext, mapping, false)
         }
+        updateEffectsKnobsButtons(context, activeDevice, effectContext)
+        sendLableApp1(activeDevice, context)
+        sendLableApp2(activeDevice, context)
     }
 
     defaultSubPage.mOnActivate = function (activeDevice, activeMapping) {
